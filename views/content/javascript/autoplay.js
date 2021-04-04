bde56938ccb24e858fe14d0e5789e2d8de80582f
highboi <highboi@protonmail.com>
2021-04-04 02:34:45 -0500

    Added CSS autoplay button to stop and start autoplaying of videos. Also added necessary JS.

--- views/content/javascript/autoplay.js
+++ views/content/javascript/autoplay.js
@@ -1,9 +1,22 @@
 //a short script to automatically play the "next" video once the current video has reached
 //the end of it's time
 
+//set the value of the input to mirror the CSS styling of the autoplay switch
+document.querySelector(".switchinput").checked = getCookie("autoplay") == "true";
+
+//handle the setting of a cookie value for the autoplay value
+document.querySelector(".switchinput").oninput = () => {
+	if (document.querySelector(".switchinput").checked) {
+		setCookie("autoplay", "true");
+	} else {
+		setCookie("autoplay", "false");
+	}
+};
+
 //set an event handler for whenever the video ends and redirect to the next video
 document.querySelector(".video-container #video").onended = () => {
-	if (typeof nextvideolink != 'undefined') {
+	//if the next video link is a defined variable
+	if (typeof nextvideolink != 'undefined' && getCookie("autoplay") == "true") {
 		window.location.href = nextvideolink;
 	}
 };
