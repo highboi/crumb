150d87b63934067005843e6001cf8b37712f0c66
highboi <highboi@protonmail.com>
2021-07-13 01:24:11 -0500

    Added regex validation for magnet links in the /setmagnet/ URL. Also the webtorrent library auto-seeds downloads for torrents, so no need to do anything on that front, webtorrents are completed.

--- servercode/torrentPath.js
+++ servercode/torrentPath.js
@@ -28,17 +28,25 @@ app.get("/setmagnet/:contentid", async (req, res) => {
 
 	console.log("CONTENT ID FOR SETTING MAGNET:", contentid);
 
-	//get data from the redis server to make sure this entry is empty
-	var magnetlink = await redisClient.getAsync(contentid);
+	//create a regex match for checking valid magnet links
+	var magnetmatch = new RegExp(/magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32}/, "i");
 
-	//do something based on if the magnet link exists or not
-	if (magnetlink == null) { //if there is no entry for this piece of content
-		//enter the new magnet link with this content id
-		redisClient.set(contentid, req.query.magnetlink);
-		res.send(true);
-	} else { //if there is an existing magnet link for this piece of content
-		//check to see that the pre-existing magnet link is ok
-		console.log("pre-existing magnet link");
+	//check for the validity of the magnet link given in the query params
+	if (req.query.magnetlink.match(magnetmatch) !== null) { //if this is a valid magnet link
+		//get data from the redis server to make sure this entry is empty
+		var magnetlink = await redisClient.getAsync(contentid);
+
+		//do something based on if the magnet link exists in the redis store or not
+		if (magnetlink == null) { //if there is no entry for this piece of content
+			//enter the new magnet link with this content id
+			redisClient.set(contentid, req.query.magnetlink);
+			res.send(true);
+		} else { //if there is an existing magnet link for this piece of content
+			console.log("pre-existing magnet link");
+			res.send(false);
+		}
+	} else { //if this is an invalid magnet link
+		console.log("INVALID MAGNET LINK");
 		res.send(false);
 	}
 });
