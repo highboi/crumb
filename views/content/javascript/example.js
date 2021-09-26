d419bd08ca8f8c2dfee5cb3d75b76ac3360b54fa
highboi <highboi@protonmail.com>
2021-09-26 06:08:29 -0500

    Installed tailwind CSS, about to begin web design. Want to implement progress bar for file uploads which will be a short switch from fetch to XMLHttpRequest when I write a function in another file.

--- views/content/javascript/example.js
+++ views/content/javascript/example.js
@@ -1,9 +1,17 @@
-document.forms[0].onsubmit = async function(e) {
-	e.preventDefault();
+var request = new XMLHttpRequest();
 
-	var file = document.getElementById("file").files[0];
+var data = new FormData();
+data.append("file", document.getElementById("file"));
 
-	var header = await getFileSignature(file);
+console.log("loading progress");
 
-	alert(`FILE HEADER: ${header}`);
+document.getElementById("button").onclick = () => {
+	request.open("POST", "/example");
+	request.send(data);
 }
+
+request.upload.addEventListener("progress", (e) => {
+	var percent_completed = (e.loaded / e.total)*100;
+
+	console.log(percent_completed);
+});
