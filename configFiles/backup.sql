e8fc11ac8ffd12178c62c441f25c9319beb8e3a7
highboi <highboi@protonmail.com>
2022-07-04 08:32:04 -0500

    Updated backup sql file.

--- configFiles/backup.sql
+++ configFiles/backup.sql
@@ -65,10 +65,10 @@ CREATE TABLE public.comments (
     likes bigint,
     dislikes bigint,
     parent_id text,
-    depth_level bigint,
-    base_parent_id text,
     reactionfile text,
-    filetype text
+    filetype text,
+    replies text,
+    deleted boolean
 );
 
 
@@ -294,8 +294,6 @@ COPY public.advertisers (id, businessdomain, businessemail, customerid) FROM std
 --
 
 COPY public.adverts (id, adlink, adfile, "position", businessid, subscriptionid, impressions) FROM stdin;
-b73f7b6fd61bae1799e0f4cef571a012	/	/adverts/1628833118711-example_banner_mobile.jpeg	square	319d6fad-2946-405b-be30-1c1be4055e0b	sub_K1wy6AvamdiQeK	182
-e37acc88dfead34efa50ad6c19bd714f	/	/adverts/1628913189660-example_banner_desktop.jpg	banner	319d6fad-2946-405b-be30-1c1be4055e0b	sub_K2IWu7KV13AchE	605
 \.
 
 
@@ -303,21 +301,10 @@ e37acc88dfead34efa50ad6c19bd714f	/	/adverts/1628913189660-example_banner_desktop
 -- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: merlin
 --
 
-COPY public.comments (user_id, comment, video_id, username, posttime, id, likes, dislikes, parent_id, depth_level, base_parent_id, reactionfile, filetype) FROM stdin;
-319d6fad-2946-405b-be30-1c1be4055e0b	another test image comment	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2021-05-17T07:24:16.153Z	2yKTqCTg	0	0	\N	0	\N	/users/comments/1621236256197-worriedpepe.jpeg	img
-319d6fad-2946-405b-be30-1c1be4055e0b	can I add a comment?	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2021-06-01T09:10:03.190Z	3QpwOhuY	0	0	\N	0	\N	\N	\N
-319d6fad-2946-405b-be30-1c1be4055e0b	TEST FOR SCROLLING	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2021-05-30T03:01:19.241Z	x190Ptqi	0	0	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	1	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	second reply to the comment	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-09-01T08:29:15.066Z	ehA8pazt	0	0	4vspNGrz	2	9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	third one for good measure	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-09-01T11:19:30.142Z	4kadKhTU	0	0	ehA8pazt	3	9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	first reply to this video comment	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-09-01T08:16:53.270Z	4vspNGrz	0	0	9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	1	9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	fourth comment for testing shit	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-09-02T04:59:30.959Z	N0ihVbfD	0	0	4vspNGrz	2	9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	This is a reply.	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-02-25T07:53:34.801Z	41d36f21-c097-4b95-b72e-459c1abe9f0c	0	0	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	1	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	This is a reply x2.	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-02-25T07:53:46.771Z	5bc8f455-9e4a-4825-8030-2e183d75280a	0	0	41d36f21-c097-4b95-b72e-459c1abe9f0c	2	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	\N	\N
-319d6fad-2946-405b-be30-1c1be4055e0b	this is another reply x3	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2021-04-10T05:53:37.572Z	ebfe86fd-6d81-4080-b11c-662c354c883c	0	0	5bc8f455-9e4a-4825-8030-2e183d75280a	3	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	\N	\N
-319d6fad-2946-405b-be30-1c1be4055e0b	Yay another one	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2021-04-10T05:54:00.043Z	5520fd4f-d036-41fc-b77a-444b29affc21	0	0	41d36f21-c097-4b95-b72e-459c1abe9f0c	2	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	This is a video comment.	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-02-25T07:54:38.214Z	9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	0	0	\N	0	\N	/users/comments/1614239678215-nyan.mp4	video
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	This is a comment.	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-02-25T07:53:21.787Z	c1e9ab0e-93bf-4560-b0d5-3bf815790fdd	0	0	\N	0	\N	\N	\N
-60235bb8-41f0-42c9-8bd7-f12f645f10a3	This is an image comment.	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	jane	2021-02-25T07:54:23.772Z	34f0ee46-ac3b-4c8a-a9a6-395b38b2157d	0	0	\N	0	\N	/users/comments/1614239663785-textreme.jpeg	img
+COPY public.comments (user_id, comment, video_id, username, posttime, id, likes, dislikes, parent_id, reactionfile, filetype, replies, deleted) FROM stdin;
+319d6fad-2946-405b-be30-1c1be4055e0b	another reply	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2022-07-01T03:02:04.964Z	pbxW32HB	0	0	blsxxT9a	\N	\N	\N	\N
+319d6fad-2946-405b-be30-1c1be4055e0b	this is a comment	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2022-07-01T03:01:45.641Z	blsxxT9a	0	0	\N	\N	\N	Qu0r1V3M,pbxW32HB,	\N
+319d6fad-2946-405b-be30-1c1be4055e0b	[DELETED]	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	example channel	2022-07-01T03:01:52.744Z	Qu0r1V3M	0	0	blsxxT9a	\N	\N	\N	\N
 \.
 
 
@@ -378,6 +365,11 @@ hello there	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be40
 hello there	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be4055e0b	16
 hello there	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be4055e0b	16
 hello there	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be4055e0b	16
+this is a live chat message	5DdR5QcN	ve9BNlwW	18
+this is another live chat message	5DdR5QcN	ve9BNlwW	24
+this is another live chat message	5DdR5QcN	ve9BNlwW	25
+this is another live chat message	5DdR5QcN	ve9BNlwW	25
+this is another live chat message	5DdR5QcN	ve9BNlwW	25
 \.
 
 
@@ -386,8 +378,9 @@ hello there	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be40
 --
 
 COPY public.playlists (user_id, name, id, videocount, candelete, private) FROM stdin;
-319d6fad-2946-405b-be30-1c1be4055e0b	example playlist	d9d55174-1250-45ef-84d9-1426c478472b	3	t	f
 60235bb8-41f0-42c9-8bd7-f12f645f10a3	janes playlist	YCMMMLlB	0	t	f
+319d6fad-2946-405b-be30-1c1be4055e0b	example playlist	d9d55174-1250-45ef-84d9-1426c478472b	4	t	f
+319d6fad-2946-405b-be30-1c1be4055e0b	another one	ZuUptkwg	1	t	f
 \.
 
 
@@ -398,8 +391,10 @@ COPY public.playlists (user_id, name, id, videocount, candelete, private) FROM s
 COPY public.playlistvideos (playlist_id, video_id, videoorder) FROM stdin;
 edca22d2-1d51-4d99-aa5d-f99f28bd8b6b	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	1
 d9d55174-1250-45ef-84d9-1426c478472b	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	1
-d9d55174-1250-45ef-84d9-1426c478472b	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	2
-d9d55174-1250-45ef-84d9-1426c478472b	bcec57c8-2eab-4acd-9f2e-24343d88c6fa	3
+d9d55174-1250-45ef-84d9-1426c478472b	bcec57c8-2eab-4acd-9f2e-24343d88c6fa	2
+d9d55174-1250-45ef-84d9-1426c478472b	HmmwavpR	3
+d9d55174-1250-45ef-84d9-1426c478472b	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	4
+ZuUptkwg	bcec57c8-2eab-4acd-9f2e-24343d88c6fa	1
 \.
 
 
@@ -455,13 +450,24 @@ $2b$10$wo4Hx.FPEvKydMybmPirYO1QSphfJk/Ermt4euUMVMQNP0QDrMke.	example channel	319
 COPY public.videofiles (id, thumbnail, video, parentid, resolution) FROM stdin;
 db5ee5c6-4dc5-4711-bd72-a166c5e47f25	/videos/thumbnails/1614151941099-bongoCat.png	/videos/files/1614151941099-nyan.mp4	\N	\N
 bcec57c8-2eab-4acd-9f2e-24343d88c6fa	/videos/thumbnails/1614229350137-bluecity.jpg	/videos/files/1614229350415-Test WebSocket Stream.webm	\N	\N
+0DNgyEcz	\N	/users/comments/1640853554720-mages.jpeg	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
+pYfyziW9	\N	/users/comments/1640854124661-nyan.mp4	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
 81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	/videos/thumbnails/1614229816950-beautiful_landscapes_in_the_world-wallpaper-1920x1080.jpg	/videos/nmsMedia/live/XpKfuO+ZsJIQ71MSfWvCcgPRksb0n2hWXe3hGahhUWU=/2021-02-24-23-10-18.mp4	\N	\N
 34f0ee46-ac3b-4c8a-a9a6-395b38b2157d	\N	/users/comments/1614239663785-textreme.jpeg	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
 9d12cc4d-2f19-46c9-9a3e-6a1d3ed42610	\N	/users/comments/1614239678215-nyan.mp4	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
+UaMGXDFc	\N	/users/comments/1638423805860-mini_cooper.jpeg	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
+ONVAsZQw	/videos/thumbnails/1638765250355-flying_blue.jpeg	/videos/files/1638765250355-nyan.mp4	\N	[144,240,360]
+fNZIcr9g	\N	/users/comments/1638765290673-magic_library.jpeg	ONVAsZQw	\N
+5DdR5QcN	/videos/thumbnails/1638765401579-floating_castle.jpeg	/videos/files/1638765401760-zach webcam stream.webm	\N	\N
 061e8b98-c896-478f-8969-2d6bb69d61a5	/videos/thumbnails/1616467261063-antarctica.png	\N	\N	\N
+a4bhGre0	\N	/users/comments/1640857644386-kraken_ship.png	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
+kghCJjbf	/videos/thumbnails/1640497458971-castle_in_the_distance.jpeg	/videos/files/1640497458971-nyan.mp4	\N	[144,240,360]
+rCh4I6JU	/videos/thumbnails/1632033327110-dune_worm.jpeg	/videos/files/1632033327358-asdf.webm	\N	\N
 cLtQABvZ	\N	/users/comments/1621236066275-worriedpepe.jpeg	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
+cw7FZvKb	\N	/users/comments/1640497514175-flying_blue.jpeg	kghCJjbf	\N
 2yKTqCTg	\N	/users/comments/1621236256197-worriedpepe.jpeg	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
 73f606a5-69a4-4471-9d37-a4dc4eed488f	/videos/thumbnails/1614534795337-spiderverse.jpeg	/videos/files/1614534795337-nyan.mp4	\N	\N
+Rk7v36HK	/videos/thumbnails/1640497632050-electric_train_bright.jpeg	/videos/files/1640497632285-example webcam stream.webm	\N	\N
 d6101b91-12f8-4c44-8595-1df400634b64	/videos/thumbnails/1616459815640-antarctica.png	\N	\N	\N
 a54cf7b7-a643-4475-86fd-cf62e9858140	/videos/thumbnails/1616048609795-city5.jpg	/videos/files/1616048609795-nyan.mp4	\N	\N
 d1b4c755-b587-4c13-b45e-b43e3ac2ccdc	/videos/thumbnails/1616048661535-city5.jpg	/videos/files/1616048661535-nyan.mp4	\N	\N
@@ -471,6 +477,7 @@ T4Kmn4yy	/videos/thumbnails/1630038825773-electric_train_bright.jpeg	\N	\N	\N
 A5WqEFt3	/videos/thumbnails/1630038976033-electric_train_bright.jpeg	\N	\N	\N
 nDUVm2bu	/videos/thumbnails/1630039055353-electric_train_bright.jpeg	\N	\N	\N
 4UA18p0C	/videos/thumbnails/1630039090387-electric_train_bright.jpeg	\N	\N	\N
+fOujGlqb	\N	/users/comments/1642715133635-nyan.mp4	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	\N
 \.
 
 
@@ -479,10 +486,14 @@ nDUVm2bu	/videos/thumbnails/1630039055353-electric_train_bright.jpeg	\N	\N	\N
 --
 
 COPY public.videos (description, thumbnail, video, title, views, id, user_id, likes, dislikes, posttime, topics, username, channelicon, streaming, enablechat, streamtype, deleted, private, subtitles) FROM stdin;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa	/videos/thumbnails/1614229816950-beautiful_landscapes_in_the_world-wallpaper-1920x1080.jpg	/videos/nmsMedia/live/XpKfuO+ZsJIQ71MSfWvCcgPRksb0n2hWXe3hGahhUWU=/2021-02-24-23-10-18.mp4	Test OBS Stream	145	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-02-25T05:10:16.950Z	test obs stream	example channel	/users/icons/1604712612118-bongoCat.png	f	t	obs	f	f	\N
-This is a test of the websocket live streaming capabilities of the site.	/videos/thumbnails/1614229350137-bluecity.jpg	/videos/files/1614229350415-Test WebSocket Stream.webm	Test WebSocket Stream	161	bcec57c8-2eab-4acd-9f2e-24343d88c6fa	60235bb8-41f0-42c9-8bd7-f12f645f10a3	0	0	2021-02-25T05:02:30.137Z	web socket websocket stream test	jane	/users/icons/1609522478769-space.png	f	t	browser	f	f	\N
-This is a test video to show the functionality of subtitles.	/videos/thumbnails/1623391587603-the_bean.jpeg	/videos/files/1623391587603-nyan.mp4	Subtitles Video Test	13	HmmwavpR	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-06-11T06:06:27.607Z	 subtitles 	example channel	/users/icons/1604712612118-bongoCat.png	f	\N	\N	f	f	/videos/subtitles1623391587603-example.srt
-This is a test video upload for the site.	/videos/thumbnails/1614151941099-bongoCat.png	/videos/files/1614151941099-nyan.mp4	Test Video Upload	527	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-02-24T07:32:21.102Z	nyan cat video example upload test	example channel	/users/icons/1604712612118-bongoCat.png	f	\N	\N	f	f	\N
+This is jeffs first webcam stream	/server/deleteicon.png			0	XUq4QMsH	XVSfYy4m	0	0	2022-01-27T04:08:10.136Z			/server/deletechannelicon.png	f	t	browser	t	f	\N
+I'm buying OBS next.	/server/deleteicon.png			0	29pTOsVG	XVSfYy4m	0	0	2022-01-27T04:09:34.207Z	obs		/server/deletechannelicon.png	f	t	obs	t	f	\N
+this is jeffs first video	/server/deleteicon.png			0	ZlaRKZev	XVSfYy4m	0	0	2022-01-27T04:05:42.163Z	 parler aws 		/server/deletechannelicon.png	f	\N	\N	t	f	
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa	/videos/thumbnails/1614229816950-beautiful_landscapes_in_the_world-wallpaper-1920x1080.jpg	/videos/nmsMedia/live/XpKfuO+ZsJIQ71MSfWvCcgPRksb0n2hWXe3hGahhUWU=/2021-02-24-23-10-18.mp4	Test OBS Stream	154	81ae5b9c-628c-48cf-a9f6-5aee7e7b64d0	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-02-25T05:10:16.950Z	test obs stream	example channel	/users/icons/1604712612118-bongoCat.png	f	t	obs	f	f	\N
+This is a test of the websocket live streaming capabilities of the site.	/videos/thumbnails/1614229350137-bluecity.jpg	/videos/files/1614229350415-Test WebSocket Stream.webm	Test WebSocket Stream	176	bcec57c8-2eab-4acd-9f2e-24343d88c6fa	60235bb8-41f0-42c9-8bd7-f12f645f10a3	0	0	2021-02-25T05:02:30.137Z	web socket websocket stream test	jane	/users/icons/1609522478769-space.png	f	t	browser	f	f	\N
+This is a test video upload for the site.	/videos/thumbnails/1614151941099-bongoCat.png	/videos/files/1614151941099-nyan.mp4	Test Video Upload	983	db5ee5c6-4dc5-4711-bd72-a166c5e47f25	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-02-24T07:32:21.102Z	nyan cat video example upload test	example channel	/users/icons/1604712612118-bongoCat.png	f	\N	\N	f	f	\N
+asdf	/server/deleteicon.png			0	wymdU1et	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2022-01-27T04:17:50.008Z	nyan		/server/deletechannelicon.png	f	t	obs	t	f	\N
+This is a test video to show the functionality of subtitles.	/videos/thumbnails/1623391587603-the_bean.jpeg	/videos/files/1623391587603-nyan.mp4	Subtitles Video Test	52	HmmwavpR	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-06-11T06:06:27.607Z	 subtitles 	example channel	/users/icons/1604712612118-bongoCat.png	f	\N	\N	f	f	/videos/subtitles1623391587603-example.srt
 This is an example video that is set to private in order to keep it hidden from other users.	/videos/thumbnails/1614534795337-spiderverse.jpeg	/videos/files/1614534795337-nyan.mp4	Example Private Video	2	73f606a5-69a4-4471-9d37-a4dc4eed488f	319d6fad-2946-405b-be30-1c1be4055e0b	0	0	2021-02-28T17:53:15.344Z	private video	example channel	/users/icons/1604712612118-bongoCat.png	f	\N	\N	f	t	\N
 \.
 
